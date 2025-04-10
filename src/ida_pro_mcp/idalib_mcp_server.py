2fda9c4e456fec68cd5c8b771ef8ffaeb655a9a6
Willi Ballenthin <willi.ballenthin@gmail.com>
2025-04-10 09:44:02 +0200

    idalib: attach parameter documentation to tool metadata

--- src/ida_pro_mcp/idalib_mcp_server.py
+++ src/ida_pro_mcp/idalib_mcp_server.py
@@ -1,8 +1,10 @@
 import sys
+import inspect
 import logging
 import argparse
 import importlib
 from pathlib import Path
+import typing_inspection.introspection as intro
 
 from rich.console import Console
 from rich.logging import RichHandler
@@ -16,6 +18,91 @@ import ida_auto
 import ida_hexrays
 
 logger = logging.getLogger(__name__)
+
+
+def fixup_tool_argument_descriptions(mcp: FastMCP):
+    # In our tool definitions within `mcp-plugin.py`, we use `typing.Annotated` on function parameters
+    # to attach documentation. For example:
+    #
+    #     def get_function_by_name(
+    #         name: Annotated[str, "Name of the function to get"]
+    #     ) -> Function:
+    #         """Get a function by its name"""
+    #         ...
+    #
+    # However, the interpretation of Annotated is left up to static analyzers and other tools.
+    # FastMCP doesn't have any special handling for these comments, so we splice them into the
+    # tool metadata ourselves here.
+    #
+    # Example, before:
+    #
+    #     tool.parameter={
+    #       properties: {
+    #         name: {
+    #           title: "Name",
+    #           type: "string"
+    #         }
+    #       },
+    #       required: ["name"],
+    #       title: "get_function_by_nameArguments",
+    #       type: "object"
+    #     }
+    #
+    # Example, after:
+    #
+    #     tool.parameter={
+    #       properties: {
+    #         name: {
+    #           title: "Name",
+    #           type: "string"
+    #           description: "Name of the function to get"
+    #         }
+    #       },
+    #       required: ["name"],
+    #       title: "get_function_by_nameArguments",
+    #       type: "object"
+    #     }
+    #
+    # References:
+    #   - https://docs.python.org/3/library/typing.html#typing.Annotated
+    #   - https://fastapi.tiangolo.com/python-types/#type-hints-with-metadata-annotations
+
+    # unfortunately, FastMCP.list_tools() is async, so we break with best practices and reach into `._tool_manager`
+    # rather than spinning up an asyncio runtime just to fetch the (non-async) list of tools.
+    for tool in mcp._tool_manager.list_tools():
+        sig = inspect.signature(tool.fn)
+        for name, parameter in sig.parameters.items():
+            # this instance is a raw `typing._AnnotatedAlias` that we can't do anything with directly.
+            # it renders like:
+            #
+            #      typing.Annotated[str, 'Name of the function to get']
+            if not parameter.annotation:
+                continue
+
+            # this instance will look something like:
+            #
+            #     InspectedAnnotation(type=<class 'str'>, qualifiers=set(), metadata=['Name of the function to get'])
+            #
+            annotation = intro.inspect_annotation(
+                                                  parameter.annotation,
+                                                  annotation_source=intro.AnnotationSource.ANY
+                                              )
+
+            # for our use case, where we attach a single string annotation that is meant as documentation,
+            # we extract that string and assign it to "description" in the tool metadata.
+
+            if annotation.type is not str:
+                continue
+
+            if len(annotation.metadata) != 1:
+                continue
+
+            description = annotation.metadata[0]
+            if not isinstance(description, str):
+                continue
+
+            logger.debug("adding parameter documentation %s(%s='%s')", tool.name, name, description)
+            tool.parameters["properties"][name]["description"] = description
 
 
 def main() -> int:
@@ -62,7 +149,7 @@ def main() -> int:
 
     if not ida_hexrays.init_hexrays_plugin():
         raise RuntimeError("failed to initialize Hex-Rays decompiler")
-    
+
     # for MCP/stdio, STDERR should not imply an error with MCP, but some clients are buggy today.
     # see: https://github.com/modelcontextprotocol/modelcontextprotocol/issues/177
     mcp = FastMCP("github.com/mrexodia/ida-pro-mcp", log_level=log_level_s, host=args.host, port=args.port)
@@ -72,6 +159,8 @@ def main() -> int:
     for name, callable in plugin.rpc_registry.methods.items():
         logger.debug("adding tool: %s: %s", name, callable)
         mcp.add_tool(callable, name)
+
+    fixup_tool_argument_descriptions(mcp)
 
     try:
         if args.transport == "sse":
@@ -87,7 +176,6 @@ def main() -> int:
         return 1
 
     return 0
-    
-    
+
 if __name__ == "__main__":
     sys.exit(main())
