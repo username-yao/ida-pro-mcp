29ed1e701f3bfd95dc66957bb6a3743ed6b12a68
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2025-04-13 16:48:24 +0200

    Remove stdio transport from idalib server

--- src/ida_pro_mcp/idalib_server.py
+++ src/ida_pro_mcp/idalib_server.py
@@ -6,10 +6,7 @@ import importlib
 from pathlib import Path
 import typing_inspection.introspection as intro
 
-from rich.console import Console
-from rich.logging import RichHandler
 from mcp.server.fastmcp import FastMCP
-from mcp.server.fastmcp.server import Settings
 
 # idapro must go first to initialize idalib
 import idapro
@@ -19,6 +16,7 @@ import ida_hexrays
 
 logger = logging.getLogger(__name__)
 
+mcp = FastMCP("github.com/mrexodia/ida-pro-mcp#idalib")
 
 def fixup_tool_argument_descriptions(mcp: FastMCP):
     # In our tool definitions within `mcp-plugin.py`, we use `typing.Annotated` on function parameters
@@ -104,33 +102,25 @@ def fixup_tool_argument_descriptions(mcp: FastMCP):
             logger.debug("adding parameter documentation %s(%s='%s')", tool.name, name, description)
             tool.parameters["properties"][name]["description"] = description
 
-
-def main() -> int:
+def main():
     parser = argparse.ArgumentParser(description="MCP server for IDA Pro via idalib")
-    parser.add_argument("--verbose", action="store_true", help="Show debug messages")
-    parser.add_argument("--transport", choices=["stdio", "sse"], default="stdio", help="MCP protocol to use")
-    # FastMCP default host is 0.0.0.0, but we should avoid exposing the server publicly by default.
-    parser.add_argument("--host", type=str, default="127.0.0.1", help="Host to serve on when using MCP/sse")
-    parser.add_argument("--port", type=int, default=Settings.model_fields["port"].default, help="Port to serve on when using MCP/sse")
+    parser.add_argument("--verbose", "-v", action="store_true", help="Show debug messages")
+    parser.add_argument("--host", type=str, default="127.0.0.1", help="Host to listen on, default: 127.0.0.1")
+    parser.add_argument("--port", type=int, default=8745, help="Port to listen on, default: 8745")
     parser.add_argument("input_path", type=Path, help="Path to the input file to analyze.")
     args = parser.parse_args()
 
     if args.verbose:
         log_level = logging.DEBUG
-        log_level_s = "DEBUG"
-
-        # this is OK for MCP/sse, but not for MCP/stdio
-        # TODO: though it *might* be ok if this is written to STDERR
         idapro.enable_console_messages(True)
     else:
         log_level = logging.INFO
-        log_level_s = "INFO"
         idapro.enable_console_messages(False)
 
-    logging.basicConfig(
-        level=log_level,
-        handlers=[RichHandler(console=Console(stderr=True))],
-    )
+    mcp.settings.log_level = logging.getLevelName(log_level)
+    mcp.settings.host = args.host
+    mcp.settings.port = args.port
+    logging.basicConfig(level=log_level)
 
     # reset logging levels that might be initialized in idapythonrc.py
     # which is evaluated during import of idalib.
@@ -150,32 +140,18 @@ def main() -> int:
     if not ida_hexrays.init_hexrays_plugin():
         raise RuntimeError("failed to initialize Hex-Rays decompiler")
 
-    # for MCP/stdio, STDERR should not imply an error with MCP, but some clients are buggy today.
-    # see: https://github.com/modelcontextprotocol/modelcontextprotocol/issues/177
-    mcp = FastMCP("github.com/mrexodia/ida-pro-mcp", log_level=log_level_s, host=args.host, port=args.port)
-
     plugin = importlib.import_module("ida_pro_mcp.mcp-plugin")
     logger.debug("adding tools...")
     for name, callable in plugin.rpc_registry.methods.items():
         logger.debug("adding tool: %s: %s", name, callable)
         mcp.add_tool(callable, name)
 
+    # NOTE: https://github.com/modelcontextprotocol/python-sdk/issues/466
     fixup_tool_argument_descriptions(mcp)
 
-    try:
-        if args.transport == "sse":
-            logger.info("MCP Server (sse) availabile at: http://%s:%d", mcp.settings.host, mcp.settings.port)
-            mcp.run(transport="sse")
-        elif args.transport == "stdio":
-            mcp.run(transport="stdio")
-        else:
-            raise ValueError("invalid transport")
-    except KeyboardInterrupt:
-        return 0
-    except Exception:
-        return 1
-
-    return 0
+    # NOTE: npx @modelcontextprotocol/inspector for debugging
+    logger.info("MCP Server (SSE) availabile at: http://%s:%d/sse", mcp.settings.host, mcp.settings.port)
+    mcp.run(transport="sse")
 
 if __name__ == "__main__":
-    sys.exit(main())
+    main()
