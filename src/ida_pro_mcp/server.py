074d7037f37b1453c7b98d8af2233ce813d3087b
Chapoly1305 <chapoly1305@gmail.com>
2025-06-06 09:54:00 -0400

    add support for Claude Code

--- src/ida_pro_mcp/server.py
+++ src/ida_pro_mcp/server.py
@@ -289,6 +289,7 @@ def install_mcp_servers(*, uninstall=False, quiet=False, env={}):
             # Claude not supported on Linux
             "Cursor": (os.path.join(os.path.expanduser("~"), ".cursor"), "mcp.json"),
             "Windsurf": (os.path.join(os.path.expanduser("~"), ".codeium", "windsurf"), "mcp_config.json"),
+            "Claude Code": (os.path.join(os.path.expanduser("~")), ".claude.json"),
         }
     else:
         print(f"Unsupported platform: {sys.platform}")
